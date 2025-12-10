98f8561fda913b9e4919e0dfecffa3012a59aae1
BugRed <o.adrian.rodrigues@gmail.com>
2025-12-09 22:09:46 -0300

    atualização do arquivo streamlit_app

--- app_frontend/streamlit_app.py
+++ app_frontend/streamlit_app.py
@@ -1,4 +1,3 @@
-# app_frontend/streamlit_app.py
 import streamlit as st
 import requests
 from PIL import Image
@@ -7,30 +6,33 @@ import os
 
 API_URL = "http://localhost:8000/predict"
 
-st.set_page_config(page_title="Iris Classifier", layout="centered")
+st.set_page_config(page_title="Penguim Classifier", layout="centered")
 
-st.title("--Iris Flower Classifier--")
+st.title("--Penguim Species Classifier--")
 st.write("Insira as medidas e obtenha a predição da espécie e a certeza (probabilidades).")
 
 # Show image
-img_path = "app_frontend/assets/iris.jpg"
+img_path = "app_frontend/assets/penguim.jpg"
 if os.path.exists(img_path):
     st.image(Image.open(img_path), caption="Iris", use_column_width=True)
 else:
-    st.info("Coloque uma imagem da flor em 'app_frontend/assets/iris.jpg' para visualizar aqui.")
+    st.info("Coloque uma imagem do penguim em 'app_frontend/assets/iris.jpg' para visualizar aqui.")
 
-st.sidebar.header("Parâmetros da flor (cm)")
-sepal_length = st.sidebar.number_input("Comprimento da sépala (sepal_length)", min_value=0.0, value=5.1, step=0.1, format="%.2f")
-sepal_width  = st.sidebar.number_input("Largura da sépala (sepal_width)", min_value=0.0, value=3.5, step=0.1, format="%.2f")
-petal_length = st.sidebar.number_input("Comprimento da pétala (petal_length)", min_value=0.0, value=1.4, step=0.1, format="%.2f")
-petal_width  = st.sidebar.number_input("Largura da pétala (petal_width)", min_value=0.0, value=0.2, step=0.1, format="%.2f")
+
+# DEFINIÇÃO DOS PARAMETROS DE ENTRADA
+st.sidebar.header("Parâmetros dos penguins:")
+flipper_length_mm = st.sidebar.number_input("Comprimento da nadadeira em mm (flipper_length_mm)", min_value=0.0, value=5.1, step=0.1, format="%.2f")
+body_mass_g  = st.sidebar.number_input("Massa corporal em g (body_mass_g)", min_value=0.0, value=3.5, step=0.1, format="%.2f")
+culmen_length_mm = st.sidebar.number_input("Comprimento da crista superior do bico em mm (culmen_length_mm)", min_value=0.0, value=1.4, step=0.1, format="%.2f")
+culmen_depth_mm  = st.sidebar.number_input("Profundidade da crista superior do bico em mm (culmen_depth_mm)", min_value=0.0, value=0.2, step=0.1, format="%.2f")
+
 
 if st.button("Prever"):
     payload = {
-        "sepal_length": sepal_length,
-        "sepal_width": sepal_width,
-        "petal_length": petal_length,
-        "petal_width": petal_width
+        "sepal_length": flipper_length_mm,
+        "sepal_width": body_mass_g,
+        "petal_length": culmen_length_mm,
+        "petal_width": culmen_depth_mm
     }
     try:
         with st.spinner("Consultando o modelo..."):
@@ -41,14 +43,14 @@ if st.button("Prever"):
             data = res.json()
             pred = data["predicted_class"]
             confidence = float(data["confidence"])
-            probs = data["probabilities"]  # [setosa, versicolor, virginica]
+            probs = data["probabilities"]  # [Adelie, Chinstrap, Gentoo]
 
             st.success(f"Predição: **{pred}**")
             st.write(f"Grau de certeza: **{confidence*100:.2f}%**")
 
             # Mostrar barras com as probabilidades
             st.subheader("Probabilidades por classe")
-            labels = ["Setosa", "Versicolor", "Virgínica"]
+            labels = ["Adelie", "Chinstrap", "Gentoo"]
             # Implementa um DataFrame para exibir
             import pandas as pd
             df = pd.DataFrame({"classe": labels, "probabilidade": probs})
